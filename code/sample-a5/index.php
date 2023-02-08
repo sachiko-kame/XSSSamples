2990755c3b3564506f41acb9a377f34c499778b6
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2023-02-09 01:32:32 +0900

    sample-a5まで

---
+++ code/sample-a5/index.php
@@ -0,0 +1,39 @@
+<?php
+$name = $_GET['name'];
+?>
+
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+
+<pre style="text-align: left; background-color: lightgray;">
+【sample】
+sample
+
+【xx_sample】
+<?php echo htmlspecialchars('</xmp><script>alert()</script>') ?><br>
+
+【XSS Locator (Polygot)】
+URL:https://cheatsheetseries.owasp.org/cheatsheets/XSS_Filter_Evasion_Cheat_Sheet.html
+※XSS Locatorで確認出来るかの確認
+</pre>
+
+<form action="./" method="get">
+    name: <input type="text" name="name">
+    <input type="submit" />
+</form>
+
+<xmp>
+<?php echo $name ?>
+</xmp>
+
+<?php echo htmlspecialchars($name) ?>
+
+</body>
+
+</html>
