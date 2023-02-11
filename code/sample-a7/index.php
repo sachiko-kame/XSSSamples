3acb4a58c01361ad910113d8d650694f41e21765
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2023-02-12 00:00:52 +0900

    sample-a7まで

---
+++ code/sample-a7/index.php
@@ -0,0 +1,39 @@
+<?php
+$name = $_GET['name'];
+?>
+
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+
+<pre style="text-align: left; background-color: lightgray;">
+【sample】
+red
+
+【xx_sample】※codeタグも同じ挙動
+<?php echo htmlspecialchars('" onmouseover="alert()'."\n") ?>
+※上記打った後は一番下の文字にマウスを持ってきてください。
+<?php echo htmlspecialchars('"><script>alert()</script>'."\n") ?>
+
+【XSS Locator (Polygot)】
+URL:https://cheatsheetseries.owasp.org/cheatsheets/XSS_Filter_Evasion_Cheat_Sheet.html
+※XSS Locatorで確認出来るかの確認
+</pre>
+
+<form action="./" method="get">
+    name: <input type="text" name="name">
+    <input type="submit" />
+</form>
+
+<div style="color: <?php echo $name /*XSS発生箇所*/ ?>">sample</div>
+
+<?php echo htmlspecialchars($name) ?>
+
+</body>
+
+</html>
