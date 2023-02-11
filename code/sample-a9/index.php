f46afc36316d5acd02b3fb6360151e423f1cf969
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2023-02-12 01:15:42 +0900

    sample-a9まで

---
+++ code/sample-a9/index.php
@@ -0,0 +1,49 @@
+<?php
+$name = $_GET['name'];
+?>
+
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+
+<pre style="text-align: left; background-color: lightgray;">
+【sample】
+https://sachiko-kame.github.io/
+
+【xx_sample】※codeタグも同じ挙動
+<?php echo htmlspecialchars('javascript:alert()'."\n") ?>
+※上記打った後は下の文字(sample)をクリックしてください。※全スキームを許可するような関数を通してしまってjavascriptのスキームが通ってしまったら
+
+【XSS Locator (Polygot)】
+URL:https://cheatsheetseries.owasp.org/cheatsheets/XSS_Filter_Evasion_Cheat_Sheet.html
+※XSS Locatorで確認出来るかの確認
+</pre>
+
+<form action="./" method="get">
+    name: <input type="text" name="name">
+    <input type="submit" />
+</form>
+
+<div id="sample"></div>
+
+<script>
+try {
+    let url = new URL("<?php echo htmlspecialchars($name) /*XSS発生箇所*/ ?>");
+    let sample = document.getElementById("sample");
+    sample.innerHTML = `<a href=${url}>sample</a>`;
+} catch (e) {
+    console.log('エラーをはきます。');
+    console.log(e);
+}
+</script>
+
+<?php echo htmlspecialchars($name) ?>
+
+</body>
+
+</html>
